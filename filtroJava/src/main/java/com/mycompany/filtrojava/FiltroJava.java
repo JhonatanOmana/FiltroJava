02ac68377c657cb0a572a7997c5c16031ad0b4e6
Jhonatan Omaña <160262647+JhonatanOmana@users.noreply.github.com>
2025-04-02 12:54:33 -0500

    Add files via upload

---
+++ filtroJava/src/main/java/com/mycompany/filtrojava/FiltroJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.filtrojava;
+
+/**
+ *
+ * @author Uniminuto Tibu
+ */
+public class FiltroJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
